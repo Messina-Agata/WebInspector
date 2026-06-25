0af2e3a2a90b46ecfbdc3f70729ac14d7b1aac94
Agata Messina <o46002009@studium.unict.it>
2026-06-25 20:16:55 +0200

    feat: disabled dependency metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
 }
 
 dependencies {
